713fae891ca92f4c12bada5ccb5a3356d9e0591f
안상현 <an359235@naver.com>
2024-06-16 10:55:33 +0000

    paddle and control

--- main.js
+++ main.js
@@ -1,42 +1,43 @@
-const ctx = document.getElementById("canvas1").getContext("2d");
+const canvas = document.getElementById("canvas1")
+const ctx = canvas.getContext("2d");
 
-// ctx.beginPath();
-// ctx.rect(20, 40, 50, 50);
-// ctx.fillStyle = "#FF0000";
-// ctx.fill();
-// ctx.closePath();
-
-// ctx.beginPath();
-// ctx.rect(160, 10, 100, 40);
-// ctx.strokeStyle = "rgb(0 0 255 / 50%)";
-// ctx.stroke();
-// ctx.closePath();
-
+//패들 변수
+let paddleHeight = 10;
+let paddleWidth = 75;
+let paddleX = (canvas.width - paddleWidth) / 2;
+let paddleY = canvas.height - paddleHeight;
+// 공 변수
 const ballRadius = 10;
 let x = canvas1.width / 2;
 let y = canvas1.height - 30;
 let dx = 2;
 let dy = -2;
-let colorCode = 0;
 
+let rightPressed = false;
+let leftPressed = false;
 
+// 공 그리기 함수
 function drawBall() {
 	ctx.beginPath();
   	ctx.arc(x, y, ballRadius, 0, Math.PI * 2);
-	if(colorCode == 0){
-		ctx.fillStyle = "#0095DD";
-	} 
-	if(colorCode == 2) {
-		ctx.fillStyle = "red";
-	}
-	
+	ctx.fillStyle = "#0095DD";
 	ctx.fill();
  	ctx.closePath();
 }
+// 패들 그리기 함수
+function drawPaddle() {
+  ctx.beginPath();
+  ctx.rect(paddleX, paddleY, paddleWidth, paddleHeight);
+  ctx.fillStyle = "#0095DD";
+  ctx.fill();
+  ctx.closePath();
+}
 
+// 게임 전체 최신화 함수
 function draw() {
 	ctx.clearRect(0, 0, canvas1.width, canvas1.height);
 	drawBall();
+	drawPaddle();
 	x += dx;
 	y += dy;
 	if(x + dx < ballRadius || x + dx > canvas1.width - ballRadius) {
@@ -44,15 +45,42 @@ function draw() {
 	}
 	if(y + dy < ballRadius || y + dy > canvas1.height - ballRadius) {
 		dy = -dy;
-		colorCode = 0;
+	}
+	
+	if (rightPressed) {
+ 		paddleX = Math.min(paddleX + 7, canvas.width - paddleWidth);
+	} else if (leftPressed) {
+ 		paddleX = Math.max(paddleX - 7, 0);
 	}
 }
-
+// 게임 시작 함수
 function startGame() {
-  const interval = setInterval(draw, 10);
+  	const interval = setInterval(draw, 10);
 }
 
+// 게임 시작 버튼 구현
 document.getElementById("runButton").addEventListener("click", function () {
   startGame();
   this.disabled = true;
 });
+
+document.addEventListener("keydown", keyDownHandler, false);
+document.addEventListener("keyup", keyUpHandler, false);
+
+function keyDownHandler(e) {
+	if (e.key === "Right" || e.key === "ArrowRight") {
+		rightPressed = true;
+	}
+	else if (e.key === "Left" || e.key === "ArrowLeft") {
+		leftPressed = true;
+	}
+}
+
+function keyUpHandler(e) {
+	if (e.key === "Right" || e.key === "ArrowLeft") {
+		rightPressed = false;
+	}
+	else if (e.key === "Left" || e.key === "ArrowLeft") {
+		leftPressed = false;
+	}
+}
